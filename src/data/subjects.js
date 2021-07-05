598532768d0bb2ac128e3082327943d27c4bd67f
Benjamin <benmneb@gmail.com>
2021-07-05 20:13:04 +0800

    begin object structure test

--- src/data/subjects.js
+++ src/data/subjects.js
@@ -30,7 +30,27 @@ export const subjects = [
 			title: 'The Earth Kasina'
 		},
 		color: brown[800],
-		details: []
+		instructions: [
+			{
+				21: [
+					'When a bhikkhu has thus severed the lesser impediments, then, on his return from his alms round after his meal and after he has got rid of drowsiness due to the meal, he should sit down comfortably in a secluded place and apprehend the sign in earth that is either made up or not made up.'
+				]
+			},
+			{
+				22: [
+					'For this is said:4 “One who is learning the earth kasiṇa apprehends the sign in earth that is either made up or not made up; that is bounded, not unbounded; limited, not unlimited; with a periphery, not without a periphery; circumscribed, not uncircumscribed; either the size of a bushel (suppa) or the size of a saucer (sarāva). He sees to it that that sign is well apprehended, well attended to, well defined. Having done that, and seeing its advantages and perceiving it as a treasure, building up respect for it, making it dear to him, he anchors his mind to that object, thinking, ‘Surely in this way I shall be freed from aging and death.’ Secluded from sense desires … he enters upon and dwells in the first jhāna …'
+				],
+				footNotes: {
+					4: '“Said in the Old Commentary. ‘One who is learning the earth kasiṇa’: one who is apprehending, grasping, an earth kasiṇa as a ‘learning sign’. The meaning is, one who is producing an earth kasiṇa that has become the sign of learning; and here ‘arousing’ should be regarded as the establishing of the sign in that way. ‘In earth’: in an earth disk of the kind about to be described. ‘Apprehends the sign’: he apprehends in that, with knowledge connected with meditative development, the sign of earth of the kind about to be described, as one does with the eye the sign of the face in a looking-glass. ‘Made up’: prepared in the manner about to be described. ‘Not made up’: in a disk of earth consisting of an ordinary threshing-floor disk, and so on. ‘Bounded’: only in one that has bounds. As regard the words ‘the size of a bushel’, etc., it would be desirable that a bushel and a saucer were of equal size, but some say that ‘the size of a saucer’ is a span and four fingers, and the ‘the size of a bushel’ is larger than that. He sees to it that that sign is well apprehended’: that meditator makes that disk of earth a well-apprehended sign. When, after apprehending the sign in it by opening the eyes, and looking and then closing them again, it appears to him as he adverts to it just as it did at the moment of looking with open eyes, then he has made it well apprehended. Having thoroughly established his mindfulness there, observing it again and again with his mind not straying outside, he sees that it is ‘well attended to’. When it is well attended to thus by adverting and attending again and again by producing much repetition and development instigated by that, he sees that it is ‘well defined’. ‘To that object’: to that object called earth kasiṇa, which has appeared rightly owing to its having been well apprehended.  ‘He anchors his mind’: by bringing his own mind to access jhāna he anchors it, keeps it from other objects” (Vism-mhṭ 119).'
+				}
+			},
+			{
+				23: [
+					'Herein, when in a previous becoming a man has gone forth into homelessness in the Dispensation or [outside it] with the rishis’ going forth and has already produced the jhāna tetrad or pentad on the earth kasiṇa, and so has such merit and the support [of past practice of jhāna] as well, then the sign arises in him on earth that is not made up, that is to say, on a ploughed area or on a threshing floor, as in the Elder Mallaka’s case.',
+					'It seems that while that venerable one was looking at a ploughed area the sign arose in him the size of that area. He extended it and attained the jhāna pentad. Then by establishing insight with the jhāna as the basis for it, he reached Arahantship.'
+				]
+			}
+		]
 	},
 	{
 		id: 2,
@@ -42,22 +62,25 @@ export const subjects = [
 			title: 'The Remaining Kasinas'
 		},
 		color: cyan[500],
-		details: [
+		instructions: [
 			{
-				id: 2.1,
-				text: 'When someone has had practice in previous [lives], the sign arises for him in water that is not made up, such as a pool, a lake, a lagoon, or the ocean as in the case of the Elder Cūḷa-Sīva. The venerable one, it seems, thought to abandon gain and honour and live a secluded life. He boarded a ship at Mahātittha (Mannar) and sailed to Jambudīpa (India). As he gazed at the ocean meanwhile, the kasiṇa sign, the counterpart of that ocean, arose in him.'
+				2.1: [
+					'When someone has had practice in previous [lives], the sign arises for him in water that is not made up, such as a pool, a lake, a lagoon, or the ocean as in the case of the Elder Cūḷa-Sīva. The venerable one, it seems, thought to abandon gain and honour and live a secluded life. He boarded a ship at Mahātittha (Mannar) and sailed to Jambudīpa (India). As he gazed at the ocean meanwhile, the kasiṇa sign, the counterpart of that ocean, arose in him.'
+				]
 			},
 			{
-				id: 2.2,
-				text: 'Someone with no such previous practice should guard against the four faults of a kasiṇa (IV.24) and not apprehend the water as one of the colours, blue, yellow, red or white. He should fill a bowl or a four-footed water pot1 to the brim with water uncontaminated by soil, taken in the open through a clean cloth [strainer], or with any other clear unturbid water. He should put it in a screened place on the outskirts of the monastery as already described and seat himself comfortably. He should neither review its colour nor bring its characteristic to mind. Apprehending the colour as belonging to its physical support, he should set his mind on the [name] concept as the most outstanding mental datum, and using any among the [various] names for water (āpo) such as “rain” (ambu), “liquid” (udaka), “dew” (vāri), “fluid” (salila),2 he should develop [the kasiṇa] by using [preferably] the obvious “water, water.”',
+				2.2: [
+					'Someone with no such previous practice should guard against the four faults of a kasiṇa (IV.24) and not apprehend the water as one of the colours, blue, yellow, red or white. He should fill a bowl or a four-footed water pot1 to the brim with water uncontaminated by soil, taken in the open through a clean cloth [strainer], or with any other clear unturbid water. He should put it in a screened place on the outskirts of the monastery as already described and seat himself comfortably. He should neither review its colour nor bring its characteristic to mind. Apprehending the colour as belonging to its physical support, he should set his mind on the [name] concept as the most outstanding mental datum, and using any among the [various] names for water (āpo) such as “rain” (ambu), “liquid” (udaka), “dew” (vāri), “fluid” (salila),2 he should develop [the kasiṇa] by using [preferably] the obvious “water, water.”'
+				],
 				footNotes: {
 					1: 'Kuṇḍika—“a four-footed water pot”: not in PED.',
 					2: 'English cannot really furnish five words for water.'
 				}
 			},
 			{
-				id: 2.3,
-				text: 'As he develops it in this way, the two signs eventually arise in him in the way already described. Here, however, the learning sign has the appearance of moving. [171] If the water has bubbles of froth mixed with it, the learning sign has the same appearance, and it is evident as a fault in the kasiṇa. But the counterpart sign appears inactive, like a crystal fan set in space, like the disk of a looking- glass made of crystal. With the appearance of that sign he reaches access jhāna and the jhāna tetrad and pentad in the way already described.'
+				2.3: [
+					'As he develops it in this way, the two signs eventually arise in him in the way already described. Here, however, the learning sign has the appearance of moving. [171] If the water has bubbles of froth mixed with it, the learning sign has the same appearance, and it is evident as a fault in the kasiṇa. But the counterpart sign appears inactive, like a crystal fan set in space, like the disk of a looking- glass made of crystal. With the appearance of that sign he reaches access jhāna and the jhāna tetrad and pentad in the way already described.'
+				]
 			}
 		]
 	},
