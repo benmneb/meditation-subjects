257cfebee538e5bc2c8f6db923ba72fc59c35f61
Benjamin <benmneb@gmail.com>
2021-07-06 15:45:56 +0800

    switch to Swipeable Drawer component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,4 @@
-import { forwardRef } from 'react';
-
-import { Dialog, Slide } from '@material-ui/core';
+import { SwipeableDrawer } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 import { ModalContents } from './index';
@@ -8,37 +6,32 @@ import { useGlobalState } from '../state';
 
 const useStyles = makeStyles((theme) => ({
 	paper: {
-		position: 'absolute',
 		height: '97%',
-		width: '100%',
-		margin: 0,
-		bottom: 0,
-		borderRadius: theme.spacing(2, 2, 0, 0),
-		maxHeight: '100%'
+		borderRadius: theme.spacing(2, 2, 0, 0)
 	}
 }));
-
-const Transition = forwardRef(function Transition(props, ref) {
-	return <Slide direction="up" ref={ref} {...props} />;
-});
 
 export default function Modal() {
 	const styles = useStyles();
 	const [state, dispatch] = useGlobalState();
 
-	function handleCloseModal() {
-		dispatch({ type: 'CHOOSE_SUBJECT', subject: null });
+	function toggleDrawer() {
+		if (state.subject) {
+			dispatch({ type: 'CHOOSE_SUBJECT', subject: null });
+		}
 	}
 
 	return (
-		<Dialog
-			maxWidth="xl"
-			open={Boolean(state.subject)}
-			TransitionComponent={Transition}
-			onBackdropClick={handleCloseModal}
+		<SwipeableDrawer
+			anchor="bottom"
 			classes={{ paper: styles.paper }}
+			disableDiscovery
+			disableSwipeToOpen
+			open={Boolean(state.subject)}
+			onClose={toggleDrawer}
+			onOpen={toggleDrawer}
 		>
 			<ModalContents data={state?.subject} />
-		</Dialog>
+		</SwipeableDrawer>
 	);
 }
