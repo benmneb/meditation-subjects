fa2070f215fee345ba28402cb1f86e7f303fcc57
Benjamin <benmneb@gmail.com>
2021-07-08 20:50:33 +0800

    give Link the option to work for non href actions

--- src/utils/Link.jsx
+++ src/utils/Link.jsx
@@ -2,6 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
 	link: {
+		cursor: 'pointer',
 		textDecoration: 'none',
 		color: theme.palette.text.primary,
 		boxShadow: `inset 0 -3px 0 ${theme.palette.primary.main}`,
@@ -11,16 +12,20 @@ const useStyles = makeStyles((theme) => ({
 				theme.palette.type === 'dark'
 					? theme.palette.primary.contrastText
 					: theme.palette.text.primary,
-			boxShadow: `inset 0 -1.15rem 0 ${theme.palette.primary.main}`
-		}
-	}
+			boxShadow: `inset 0 -1.15rem 0 ${theme.palette.primary.main}`,
+		},
+	},
 }));
 
-export default function Link({ children, ...props }) {
+export default function Link({ children, noHref, ...props }) {
 	const styles = useStyles();
 
+	function handleClick(e) {
+		if (noHref) e.preventDefault();
+	}
+
 	return (
-		<a className={styles.link} {...props}>
+		<a className={styles.link} onClick={(e) => handleClick(e)} {...props}>
 			{children}
 		</a>
 	);
