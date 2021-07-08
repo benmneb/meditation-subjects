c772a1b9085c9dd1518232c8ad66941cb44a0f29
Benjamin <benmneb@gmail.com>
2021-07-08 16:52:17 +0800

    seperate instructions data

---
+++ src/data/instructions/earthKasina.js
@@ -0,0 +1,21 @@
+export const earthKasina = [
+	{
+		21: [
+			'When a bhikkhu has thus severed the lesser impediments, then, on his return from his alms round after his meal and after he has got rid of drowsiness due to the meal, he should sit down comfortably in a secluded place and apprehend the sign in earth that is either made up or not made up.'
+		]
+	},
+	{
+		22: [
+			'For this is said:4 “One who is learning the earth kasiṇa apprehends the sign in earth that is either made up or not made up; that is bounded, not unbounded; limited, not unlimited; with a periphery, not without a periphery; circumscribed, not uncircumscribed; either the size of a bushel (suppa) or the size of a saucer (sarāva). He sees to it that that sign is well apprehended, well attended to, well defined. Having done that, and seeing its advantages and perceiving it as a treasure, building up respect for it, making it dear to him, he anchors his mind to that object, thinking, ‘Surely in this way I shall be freed from aging and death.’ Secluded from sense desires … he enters upon and dwells in the first jhāna …'
+		],
+		footNotes: {
+			4: '“Said in the Old Commentary. ‘One who is learning the earth kasiṇa’: one who is apprehending, grasping, an earth kasiṇa as a ‘learning sign’. The meaning is, one who is producing an earth kasiṇa that has become the sign of learning; and here ‘arousing’ should be regarded as the establishing of the sign in that way. ‘In earth’: in an earth disk of the kind about to be described. ‘Apprehends the sign’: he apprehends in that, with knowledge connected with meditative development, the sign of earth of the kind about to be described, as one does with the eye the sign of the face in a looking-glass. ‘Made up’: prepared in the manner about to be described. ‘Not made up’: in a disk of earth consisting of an ordinary threshing-floor disk, and so on. ‘Bounded’: only in one that has bounds. As regard the words ‘the size of a bushel’, etc., it would be desirable that a bushel and a saucer were of equal size, but some say that ‘the size of a saucer’ is a span and four fingers, and the ‘the size of a bushel’ is larger than that. He sees to it that that sign is well apprehended’: that meditator makes that disk of earth a well-apprehended sign. When, after apprehending the sign in it by opening the eyes, and looking and then closing them again, it appears to him as he adverts to it just as it did at the moment of looking with open eyes, then he has made it well apprehended. Having thoroughly established his mindfulness there, observing it again and again with his mind not straying outside, he sees that it is ‘well attended to’. When it is well attended to thus by adverting and attending again and again by producing much repetition and development instigated by that, he sees that it is ‘well defined’. ‘To that object’: to that object called earth kasiṇa, which has appeared rightly owing to its having been well apprehended.  ‘He anchors his mind’: by bringing his own mind to access jhāna he anchors it, keeps it from other objects” (Vism-mhṭ 119).'
+		}
+	},
+	{
+		23: [
+			'Herein, when in a previous becoming a man has gone forth into homelessness in the Dispensation or [outside it] with the rishis’ going forth and has already produced the jhāna tetrad or pentad on the earth kasiṇa, and so has such merit and the support [of past practice of jhāna] as well, then the sign arises in him on earth that is not made up, that is to say, on a ploughed area or on a threshing floor, as in the Elder Mallaka’s case.',
+			'It seems that while that venerable one was looking at a ploughed area the sign arose in him the size of that area. He extended it and attained the jhāna pentad. Then by establishing insight with the jhāna as the basis for it, he reached Arahantship.'
+		]
+	}
+];
