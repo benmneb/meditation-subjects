205fcdd2af21a5985a1c8e774e386a4416486fc0
Benjamin <benmneb@gmail.com>
2021-07-15 19:56:48 +0800

    make redux dev tools only show in development

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import App from './App';
 
 const store = createStore(
 	reducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 ReactDOM.render(
