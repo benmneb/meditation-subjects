a34fc3428c3c12f52c0660295c486b18f4575cea
benmneb <62878664+benmneb@users.noreply.github.com>
2022-07-03 12:31:19 +0800

    chore: finish migration from `@mui/styles` to v5, and remove StyledEngineProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { CssBaseline } from '@mui/material'
-import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles'
+import { ThemeProvider } from '@mui/material/styles'
 
 import { useTheme } from './assets'
 import {
@@ -13,24 +13,20 @@ import {
 	FootnoteDialog,
 } from './components'
 
-import heroImg from './assets/hero.jpg'
-
 export default function App() {
 	const theme = useTheme()
 
 	return (
-		<StyledEngineProvider injectFirst>
-			<ThemeProvider theme={theme}>
-				<CssBaseline />
-				<Hero imageSrc={heroImg} />
-				<TopBar />
-				<AboutDrawer />
-				<FiltersDrawer />
-				<List />
-				<Footer />
-				<SubjectDrawer />
-				<FootnoteDialog />
-			</ThemeProvider>
-		</StyledEngineProvider>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<Hero />
+			<TopBar />
+			<AboutDrawer />
+			<FiltersDrawer />
+			<List />
+			<Footer />
+			<SubjectDrawer />
+			<FootnoteDialog />
+		</ThemeProvider>
 	)
 }
